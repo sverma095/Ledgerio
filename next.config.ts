27dadf83f1d8ec50c1d717dcb5cad324583e1a53
sverma095 <svtiger543939@gmail.com>
2026-07-23 15:44:16 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,37 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
+
+  trailingSlash: true,
+
   async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
         ],
       },
     ];
