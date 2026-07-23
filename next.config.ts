e28c942d0edfad52231e1e8c5555d267788e35a6
sverma095 <svtiger543939@gmail.com>
2026-07-23 15:56:34 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,32 +11,6 @@ const nextConfig: NextConfig = {
   },
 
   trailingSlash: true,
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
